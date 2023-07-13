6b879a5b8e368ef8d4da72b4b870b6a39c418d20
KeadenM <keadenmeacham@gmail.com>
2023-07-12 18:50:29 -0600

    created functions to create the ReadMe and the questions to fill it out.

--- index.js
+++ index.js
@@ -1,16 +1,103 @@
-// TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
 const {licenseBadge, LicenseLink} = require ('./utils/generateMarkdown.js')
 
-// TODO: Create an array of questions for user input
-const questions = [];
+function createReadMe(name, description, installation, usage, contribute, tests, github, email, license) {
+    const renderLicenseBadge = licenseBadge(license);
+    const renderLicenseLink =licenseLink(license);
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+    return `
+# ${name}
 
-// TODO: Create a function to initialize app
-function init() {}
+${renderLicenseBadge}
+    
 
-// Function call to initialize app
+## Description
+${description}
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+- [Questions](#questions)
+
+## Installation
+${installation}
+
+## Usage
+${usage}
+
+## Contributing
+${contribute}
+
+## Tests
+${tests}
+
+## Questions
+If you have any questions my github is: ${github}. You can also reach me via email at: ${email}
+
+## License
+${license} ${renderLicenseLink}
+`;
+}
+
+function init() {
+    inquirer
+    .prompt([
+      {
+        type: 'input',
+        name: 'name',
+        message: 'What is the name of your project/app?'
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Please provide a description.'
+      },
+      {
+        type: 'imput',
+        name: 'installation',
+        message: 'Please provide installation instructions.'
+      },
+      {
+        type: 'imput',
+        name: 'usage',
+        message: 'Please provide app/project usage.'
+      },
+      {
+        type: 'imput',
+        name: 'tests',
+        message: 'Provide any tests for your application.'
+      },
+      {
+        type: 'imput',
+        name: 'github',
+        message: 'Please provide your github for user questions'
+      },
+      {
+        type: 'imput',
+        name: 'email',
+        message: 'Please provide your email for user questions'
+      },
+      {
+        type: 'list',
+        name: 'license',
+        message: 'Please select a license',
+        choices: ['MIT', 'Apache', 'GNU General', 'BSD', 'GNU Lesser', 'None'],
+      },
+    ]).then((answers) => {
+        const { name, description, installation, usage, contribute, tests, github, email, license } = answers;
+        const readMeContent = createReadMe(name, description, installation, usage, contribute, tests, github, email, license);
+      
+        fs.writeFile('README.md', readMeContent, (err) => {
+          if (err) {
+            console.error('Error writing file:', err);
+          } else {
+            console.log('README.MD created successfully.');
+          }
+        });
+    });
+}
+
 init();
